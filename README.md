92d3d636faee70fde5ad11cd89e326c427d2793d
dheerajtp <dheeraj@xltech.in>
2022-12-10 13:04:11 +0530

    form validation using  react-hook-form and yup

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# Tenth Chapter
+# Eleventh Chapter
 
-    ## React Query
+    ## React Hook Forms and Yup Validation
 
-    - install react query using the command npm i @tanstack/react-query
-    - Import QueryClient and QueryClientProvider from @tanstack/react-query
-    - Create a client with let client = new QueryClient();
-    - Pass client to QueryClientProvider
-    - useQuery is used to make request.
+    - Install react-hook-form and yup package
+    - Import useForm from react-hook-form
+    - To use hook grap register and handleSubmit from useForm
+    - Create a function to submit the data and pass this function to handleSubmit
+    - register function is used in each of the input to tell react hook form which validation should be applied to the input.
+    - We will use yupResolver from @hookform/resolvers to pass the schema we generated for the validation of our input into the useForm.
--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import "./App.css";
-import { useState, createContext } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import Contact from "./pages/Contact";
-import Error from "./pages/Error";
-import Nav from "./components/Nav";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-export const AppContext = createContext();
+import Form from "./Form";
 
 function App() {
-  const [username, setUserName] = useState("user 1");
-  const client = new QueryClient();
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <AppContext.Provider value={{ username, setUserName }}>
-          <Router>
-            <Nav />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="*" element={<Error />} />
-            </Routes>
-          </Router>
-        </AppContext.Provider>
-      </QueryClientProvider>
+      <Form />
     </div>
   );
 }
